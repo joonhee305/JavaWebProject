2cc56ec1ad2e4e244e2176d3bfbc33811d394e02
joonhee305 <74956803+joonhee305@users.noreply.github.com>
2022-04-07 23:49:51 +0900

    new file

---
+++ server1.js
@@ -0,0 +1,18 @@
+const http=require('http');
+const fs=require('fs').promises;
+
+const server=http.createServer(async (req,res)=>{
+    try{
+        const data=await fs.readFile('./server2.html');
+        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+        res.end(data);
+    }catch(err){
+        console.error(err);
+        res.writeHead(500,{'Content-Type':'text/plain; charset=utf-8'});
+        res.end('어라 실패?');
+    }
+})
+
+server.listen(8080,()=>{
+    console.log('자 드가자');
+})
